fd17fa6a9e43a8ad36c924e743d1ab5205e6c3fc
aloisdg <aloisdegouvello@live.fr>
2023-03-04 11:26:30 +0100

    Refactor js

--- script.js
+++ script.js
@@ -13,40 +13,35 @@ const isDotInLowerLetters = (source, c, i) =>
   isLowerLetter(source[i - 1]) &&
   isLowerLetter(source[i + 1]);
 
-const replaceDots = (source) => [...source]
-  .reduce((result, c, i) => result + (isDotInLowerLetters(source, c, i) ? interpunkt : c),  "");
+const replaceDot = (source, c, i) => isDotInLowerLetters(source, c, i) ? interpunkt : c;
 
-const updateResult = (source) => {
-  resultElement.value = replaceDots(source);
-};
+const replaceDots = (source) => [...source]
+  .reduce((result, c, i) => result + replaceDot(source, c, i), "");
+
+const randomTimeout = () => Math.floor(Math.random() * 42) + 4.2;
+
+const copy = async (content) => await navigator.clipboard.writeText(content);
 
 const sourceElement = document.querySelector("#source");
 const resultElement = document.querySelector("#result");
 
-sourceElement.oninput = (e) => updateResult(e.target.value);
+sourceElement.oninput = (e) => resultElement.value = replaceDots(e.target.value);
 
-const randomTimeout = () => Math.floor(Math.random() * 42) + 4.2;
-
-// inspiration from https://w3collective.com/animated-typing-text-effect/
 const sourcePlaceholder = sourceElement.placeholder;
 sourceElement.placeholder = "";
 
 document.addEventListener("DOMContentLoaded", () => {
-  let letter = 0;
+  let index = 0;
   const typeText = () => {
-    if (letter >= sourcePlaceholder.length) return;
-    sourceElement.placeholder += sourcePlaceholder.charAt(letter);
-    resultElement.placeholder = replaceDots(sourceElement.placeholder);
-    letter++;
+    if (index >= sourcePlaceholder.length) return;
+    const char = sourcePlaceholder.charAt(index);
+    sourceElement.placeholder += char;
+    resultElement.placeholder += replaceDot(sourcePlaceholder, char, index);
+    index++;
     setTimeout(typeText, randomTimeout());
   };
   typeText();
 });
 
-const copy = async (content) => {
-  return await navigator.clipboard.writeText(content);
-};
-
-document.querySelector("#copy").onclick = () =>
-  copy(document.querySelector("#result").value);
+document.querySelector("#copy").onclick = () => copy(resultElement.value);
 document.querySelector("#copyPoint").onclick = () => copy(interpunkt);
